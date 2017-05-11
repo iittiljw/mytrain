2a7697169c0d741895f594e394a3c156eab1eac5
root <root@Aliyun_Myhost_patrick.(none)>
2017-05-11 20:09:07 +0800

    ex03,ex04

---
+++ python/ex03.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+############
+#ex03:IF
+############
+
+a = 0
+if  a < 5:
+    print a
+else:
+    print "A<5"
---
+++ python/ex04.py
@@ -0,0 +1,16 @@
+#coding:utf8
+
+
+#################
+#ex04: argv
+################
+
+from sys import argv
+script, first, second, third = argv
+
+
+print "The script is call:",script
+print "The first variable is:",first
+print "The second variable is:",second
+print "The third variable is:",third
+
