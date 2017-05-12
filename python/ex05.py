554f1332080bcb3547c0cbfa7c214f64c0c5c605
root <root@Aliyun_Myhost_patrick.(none)>
2017-05-12 15:13:24 +0800

    add ex05

---
+++ python/ex05.py
@@ -0,0 +1,19 @@
+# -*- coding:utf8 -*-
+
+###########
+#
+#ex05:while语句
+#
+############
+
+
+i = 1
+k = 1
+
+while i <= 9:
+    while k<= i:
+        print i,'*',k,'=',k*i,"    ",
+        k += 1
+    print ""
+    k = 1
+    i +=1
